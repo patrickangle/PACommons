6d1474e10d164d8b5c7b279533c0965c4c35cad1
patrickangle <patrickangle@macbook-pro.localdomain>
2018-08-11 19:18:06 -0400

    - Added new MenuItems utility class to allow for concise creation of common JMenuItem configurations.

---
+++ src/main/java/com/patrickangle/commons/util/MenuItems.java
@@ -0,0 +1,54 @@
+/*
+ * Patrick Angle Commons Library
+ * Copyright 2018 Patrick Angle
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.patrickangle.commons.util;
+
+import java.awt.Toolkit;
+import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+
+/**
+ *
+ * @author patrickangle
+ */
+public class MenuItems {
+    public static final int SYSTEM_MODIFIER = Toolkit.getDefaultToolkit().getMenuShortcutKeyMaskEx();
+    public static final int ALT_MOFIFIER = InputEvent.ALT_DOWN_MASK;
+    public static final int SHIFT_MODIFIER = InputEvent.SHIFT_DOWN_MASK;
+    
+    public static final int SYSTEM_ALT_MODIFIER = SYSTEM_MODIFIER | ALT_MOFIFIER;
+    public static final int SYSTEM_SHIFT_MODIFIER = SYSTEM_MODIFIER | SHIFT_MODIFIER;
+    public static final int SYSTEM_ALT_SHIFT_MODIFIER = SYSTEM_MODIFIER | ALT_MOFIFIER | SHIFT_MODIFIER;
+    
+    public static JMenuItem item(String title, ActionListener actionListener) {
+        JMenuItem menuItem = new JMenuItem(title);
+        menuItem.addActionListener(actionListener);
+        return menuItem;
+    }
+    
+    public static JMenuItem item(String title, char acceleratorCharacter, int acceleratorModifier, ActionListener actionListener) {
+        return MenuItems.item(title, KeyStroke.getKeyStroke(acceleratorCharacter, acceleratorModifier), actionListener);
+    }
+    
+    public static JMenuItem item(String title, KeyStroke acceleratorKeystroke, ActionListener actionListener) {
+        JMenuItem menuItem = new JMenuItem(title);
+        menuItem.addActionListener(actionListener);
+        menuItem.setAccelerator(acceleratorKeystroke);
+        return menuItem;
+    }
+}
