e389bace6a7f04f42f0449247b9cc2155c8fcfa2
Patrick Angle <patrick.angle@worldstage.com>
2018-09-02 18:13:06 -0400

    - Resolved incorrect license header attached to file.

--- src/main/java/com/patrickangle/commons/logging/Logging.java
+++ src/main/java/com/patrickangle/commons/logging/Logging.java
@@ -1,13 +1,18 @@
 /*
- * WorldStage Waltz
+ * Patrick Angle Commons Library
+ * Copyright 2018 Patrick Angle
  * 
- * Copyright © 2018 WorldStage.
- * All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * 
- * Unauthorized tampering, copying, or distribution of this file,
- *   via any medium, is strictly prohibited.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  * 
- * The contents of this file are proprietary and confidential.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.patrickangle.commons.logging;
 
