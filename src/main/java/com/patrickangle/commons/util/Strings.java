63fe8ea0f8bc06adeb98a594fe9632786387e04d
Patrick Angle <patrick.angle@worldstage.com>
2018-11-19 12:09:11 -0500

    - Added utility method to Strings class for splitting strings by spaces, taking into account spaces within quotation marks being ignored.

--- src/main/java/com/patrickangle/commons/util/Strings.java
+++ src/main/java/com/patrickangle/commons/util/Strings.java
@@ -17,12 +17,37 @@
 package com.patrickangle.commons.util;
 
 import com.patrickangle.commons.Pair;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
  * @author Patrick Angle
  */
 public class Strings {
+    private static Pattern SPLIT_BY_UNESCAPED_WHITESPACE_REGEX = Pattern.compile("[^\\s\"']+|\"([^\"]*)\"|'([^']*)'");
+    
+    
+    public static List<String> splitByUnescapedWhitespace(String string) {
+        List<String> matchList = new ArrayList<String>();
+        Matcher regexMatcher = SPLIT_BY_UNESCAPED_WHITESPACE_REGEX.matcher(string);
+        while (regexMatcher.find()) {
+            if (regexMatcher.group(1) != null) {
+                // Add double-quoted string without the quotes
+                matchList.add(regexMatcher.group(1));
+            } else if (regexMatcher.group(2) != null) {
+                // Add single-quoted string without the quotes
+                matchList.add(regexMatcher.group(2));
+            } else {
+                // Add unquoted word
+                matchList.add(regexMatcher.group());
+            }
+        }
+        
+        return matchList;
+    }
 
     public static Pair<String, Integer> trailingIntegerComponents(String string) {
         int offset = string.length();
@@ -41,14 +66,15 @@ public class Strings {
         }
         return Pair.makePair(string.substring(0, offset), Integer.parseInt(string.substring(offset)));
     }
-    
+
     public static String incrementTrailingInteger(String string, int defaultStartingNumber) {
         Pair<String, Integer> components = Strings.trailingIntegerComponents(string);
-        
+
         if (components.b == Integer.MIN_VALUE) {
             return components.a + " " + defaultStartingNumber;
         } else {
             return components.a + (components.b + 1);
         }
     }
+
 }
